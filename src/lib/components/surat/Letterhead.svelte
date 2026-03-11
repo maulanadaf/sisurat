61a73817b3abce4b29456d5b113d6befcc4b2315
maulanadaf <maulanadafli10@gmail.com>
2026-03-12 01:33:30 +0700

    upduate

--- src/lib/components/surat/Letterhead.svelte
+++ src/lib/components/surat/Letterhead.svelte
@@ -1,59 +1,106 @@
 <script>
-    import logoItb from "$lib/assets/logo-itb.png";
-    import logoUkm from "$lib/assets/logo.png";
+  import logoItb from "$lib/assets/logo-itb.png";
+  import logoUkm from "$lib/assets/logo.png";
 </script>
 
 <!-- Letterhead / Kop Surat -->
-<div class="kop-surat w-full font-serif">
-    <!-- Baris Atas: Logo + Teks + Logo -->
-    <!-- Baris Atas: Grid 3 Kolom untuk keseimbangan sejati (Logo - Teks - Logo) -->
-    <div class="grid grid-cols-[100px_1fr_100px] items-center gap-4 pb-2">
-        <!-- Logo Kiri: ITB Yadika -->
-        <img
-            src={logoItb}
-            alt="Logo ITB Yadika Pasuruan"
-            class="w-full h-auto object-contain"
-        />
-
-        <!-- Teks Tengah -->
-        <div class="text-center flex flex-col justify-center w-full">
-            <p
-                class="font-bold tracking-wide uppercase text-black"
-                style="font-family: Cambria, Georgia, serif; font-size: 18pt; line-height: 1;"
-            >
-                UKM Kewirausahaan
-            </p>
-            <p
-                class="font-extrabold tracking-wide uppercase text-black"
-                style="font-family: Cambria, Georgia, serif; font-size: 22pt; line-height: 1; margin-top: 1px;"
-            >
-                ITB Yadika Pasuruan
-            </p>
-            <p
-                class="font-bold uppercase whitespace-nowrap text-black"
-                style="font-family: Calibri, 'Gill Sans', sans-serif; font-size: 12pt; line-height: 1.1; margin-top: 1px;"
-            >
-                Fakultas Teknologi Informasi &ndash; Fakultas Hukum dan Bisnis
-            </p>
-            <p
-                class="text-black"
-                style="font-family: 'Times New Roman', Times, serif; font-size: 10pt; line-height: 1; margin-top: 2px;"
-            >
-                Jl. Salem No. 3 Kersikan Telp. (0343) 746000 &ndash; 748914
-                Bangil 67153
-            </p>
-        </div>
-
-        <!-- Logo Kanan: UKM Kewirausahaan -->
-        <img
-            src={logoUkm}
-            alt="Logo UKM Kewirausahaan ITB Yadika Pasuruan"
-            style="width: 200px; height: auto;"
-            class="object-contain"
-        />
+<div class="kop-surat font-serif text-black -mx-4 md:-mx-8">
+  <div class="flex justify-between items-center gap-2 pb-2 w-full">
+    <!-- Logo Kiri: ITB Yadika -->
+    <div class="w-[90px] md:w-[100px] shrink-0 flex-none">
+      <img
+        src={logoItb}
+        alt="Logo ITB Yadika Pasuruan"
+        class="w-full h-auto object-contain"
+      />
     </div>
 
-    <!-- Garis Pemisah -->
-    <div class="border-black" style="border-top-width: 3px;"></div>
-    <div class="border-black mt-0.5" style="border-top-width: 1px;"></div>
+    <!-- Teks Tengah -->
+    <div
+      class="grow flex flex-col items-center justify-center text-center px-1 min-w-0"
+    >
+      <h2
+        class="font-bold tracking-wide uppercase text-black"
+        style="font-family: 'Times New Roman', Times, serif; font-size: 11pt; line-height: 1.1; color: black;"
+      >
+        Institut Teknologi & Bisnis Yadika Pasuruan
+      </h2>
+      <h1
+        class="font-bold tracking-wide uppercase text-black whitespace-nowrap"
+        style="font-family: 'Times New Roman', Times, serif; font-size: 14pt; line-height: 1.1; color: black;"
+      >
+        Unit Kegiatan Mahasiswa Kewirausahaan
+      </h1>
+
+      <div
+        class="flex justify-center w-full mt-1.5 mb-1"
+        style="font-family: 'Times New Roman', Times, serif;"
+      >
+        <!-- Kolom Kiri -->
+        <div class="flex-1 flex flex-col items-center">
+          <div class="inline-block">
+            <p
+              class="font-bold underline uppercase text-[9pt] text-center w-full"
+            >
+              Fakultas Teknologi Informasi
+            </p>
+            <div
+              class="mt-0 text-[9pt] leading-none flex flex-col items-start pl-1"
+            >
+              <p>- SISTEM & TEKNOLOGI INFORMASI (S1)</p>
+              <p class="-mt-[2px]">- SISTEM INFORMASI (S1)</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Kolom Kanan -->
+        <div class="flex-1 flex flex-col items-center">
+          <div class="inline-block">
+            <p
+              class="font-bold underline uppercase text-[9pt] text-center w-full"
+            >
+              Fakultas Hukum Dan Bisnis
+            </p>
+            <div
+              class="mt-0 text-[9pt] leading-none flex flex-col items-start pl-1"
+            >
+              <p>- MANAJEMEN (S1)</p>
+              <p class="-mt-[2px]">- AKUNTANSI (S1)</p>
+              <p class="-mt-[2px]">- HUKUM BISNIS (S1)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Logo Kanan: UKM Kewirausahaan -->
+    <div class="w-[90px] md:w-[100px] shrink-0 flex-none">
+      <img
+        src={logoUkm}
+        alt="Logo UKM Kewirausahaan ITB Yadika Pasuruan"
+        class="w-full h-auto object-contain"
+      />
+    </div>
+  </div>
+
+  <p
+    style="font-family: 'Times New Roman', Times, serif; font-size: 9pt; line-height: 1.3;"
+    class="text-center w-full -mt-2 pb-1 whitespace-nowrap text-black"
+  >
+    Jl. Salem no 03 Kersikan, Telp (0343) 749266 &ndash; 087715728111, Bangil
+    &ndash; Pasuruan 67153, <i class="italic">email:</i>
+    <a
+      href="mailto:mail@itbyadika.ac.id"
+      class="text-blue-700 underline decoration-blue-700"
+      >mail@itbyadika.ac.id</a
+    >, <i class="italic">site:</i><a
+      href="http://www.itbyadika.ac.id"
+      target="_blank"
+      class="text-blue-700 underline decoration-blue-700">www.itbyadika.ac.id</a
+    >
+  </p>
+
+  <!-- Garis Pemisah -->
+  <div class="border-black w-full" style="border-top-width: 3px;"></div>
+  <div class="border-black mt-px w-full" style="border-top-width: 1px;"></div>
 </div>
